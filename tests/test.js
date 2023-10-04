4bb7fd474ce7cb5e29e4f8a44bc887d354db290e
Romain Surand <91885986+RSurandDS@users.noreply.github.com>
2023-10-04 09:00:43 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
 describe('Basic Test Suite', () => {
-  it('should return "Hello, World!" when accessing the root path', () => {
-    const expectedResult = 'Hello, World!';
-    const actualResult = 'Hello, World!'; // Replace with your application's logic
+  it('should return "My name is Roro" when accessing the root path', () => {
+    const expectedResult = 'My name is Roro';
+    const actualResult = 'My name is Roro'; // Replace with your application's logic
 
     assert.strictEqual(actualResult, expectedResult);
   });
